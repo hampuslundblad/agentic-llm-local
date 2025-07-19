bf98dec7e8e33ec0dffba57ace707045e1511afe
Hampus Lundblad <hampus.lundblad@hotmail.se>
2025-07-19 13:04:31 +0200

    add examples

---
+++ prompts/examples.py
@@ -0,0 +1,5 @@
+examples = [
+"""Abel controls Underworld Breach. Can they dash Pitiless Horde from their graveyard? 
+No. Escape and dash are both alternative costs (702.138a, 702.109a), and only one alternative cost can be paid to cast a spell. (118.9a)
+""" 
+]
